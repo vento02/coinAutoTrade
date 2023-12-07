175dbef4116ed716409f477f16ffdfb10a72b744
humpose <144011010+humpose@users.noreply.github.com>
2023-12-07 20:05:31 +0900

    Feat: slack 명령어 기능 추가/잔고 확인 메시지 기능 추가
    command.py는 명령어에 따라서 특정 기능을 수행하는 기능이고, balance.py는 잔고를 확인해 slack으로 메시지를 보내는 기능입니다.
    가빈 누나는 현재 github에 업로드된 RSI파일과 기존 돌파매매법 파일을 하나로 합쳐주시고, 그 파일에 제가 업로드하는 두 파일을 넣어주세요.
    그리고, command.py코드를 실행하기 전에 https://yunwoong.tistory.com/135 이 링크로 들어가서 사전 준비를 해주세요.
    마지막으로, command.py의 전역변수 token과 absolutePath를 상황에 맞게 바꿔주세요. 이해가 안 되거나 막히는 부분이 있다면 전화주세요.

---
+++ balance.py
@@ -0,0 +1,51 @@
+
+import pyupbit
+import datetime
+import numpy as np
+import os
+import requests
+
+access = os.environ['access']                           # Upbit API access 키
+secret = os.environ['secret']                           # Upbit API secret 키
+myToken = os.environ["Slack_Token"]  # Access Token
+myChannel = "비트코인-자동매매"  # 채널 이름 OR 채널 ID
+# 내 잔고 조회_시작
+def get_balance(ticker):
+      balances = upbit.get_balances()
+      for b in balances:
+          if b['currency'] == ticker:
+              if b['balance'] is not None:
+                  return float(b['balance'])
+              else:
+                  return 0
+# 내 잔고 조회_끝
+
+def post_message(token, channel, text):
+    response = requests.post(
+        "https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer " + token},
+        data={"channel": channel, "text": text},
+    )
+    print(response)
+
+
+# 로그인_시작
+try:
+  upbit = pyupbit.Upbit(access, secret)
+  my_Balance = get_balance("KRW")             # 내 잔고
+  print("Login OK")
+  print("==========Autotrade start==========")
+except:
+  print("!!Login ERROR!!")
+# 로그인_끝
+else:
+  print("내 잔고 : "+str(format(int(my_Balance),','))+" 원")
+  print("date:"+str(datetime.datetime.now()))
+
+post_message(myToken, myChannel, " ")
+post_message(
+    myToken,
+    myChannel,
+    "내 잔고 : "+str(format(int(my_Balance),','))+" 원" + "\n" + "date:"+str(datetime.datetime.now()),
+    )
+
